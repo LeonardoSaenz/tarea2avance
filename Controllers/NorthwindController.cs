653c51fc15f305f7832e46367d35abf74243dcfc
LeonardoSaenz <leonardorsaenz@gmail.com>
2021-03-21 17:43:26 -0600

    first commit

---
+++ Controllers/NorthwindController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea1DWBE.DataAccess;
+
+namespace Tarea1DWBE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NorthwindController : Controller
+    {        
+        // GET: NorthwindController
+        [HttpGet]
+        public IEnumerable<Categories> Get()
+        {
+            using (var dataContext = new NorthwindContext())
+            {
+                var employeeQuery = dataContext.Categories.Select(s => s);
+                var output = employeeQuery.ToList();
+                return output;
+            }
+        }
+    }
+}
---
+++ Services/CategoriesS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea1DWBE.DataAccess;
+
+namespace Tarea1DWBE.Services
+{
+    public class CategoriesS
+    {
+        NorthwindContext dataContext = new NorthwindContext();
+        public IQueryable<Categories> GetAllCategories()
+        {
+            return dataContext.Categories.Select(s => s);
+        }
+    }
+
+
+
+}
